9869bdd818b504b523811d2a90a040ff21858aaf
Dimitris Tetepoulidis <48908714+jimtete@users.noreply.github.com>
2020-06-27 15:52:52 +0300

    not mine but I am cleaning up my PC

---
+++ Allilepidrasi/Erg11/AirTeith/src/Flights.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author dmarina
+ */
+public class Flights {
+    String flightFrom;
+    String flightTo;
+    int flightPrice;
+       
+
+    Flights (String F, String T, int P){
+        flightFrom = F;
+        flightTo = T;
+        flightPrice = P;
+    }
+} 
---
+++ Allilepidrasi/Erg4/JavaApplication2/src/Presets.java
@@ -0,0 +1,16 @@
+public class Presets 
+{
+    int bass ; 
+    int midrange ;
+    int tremble ; 
+    int balance ; 
+    int volume ;
+    public Presets(int a, int b, int c, int d, int e) 
+    {
+        this.bass = a ; 
+        this.midrange = b ; 
+        this.tremble = c ; 
+        this.balance = d ;
+        this.volume = e ; 
+    }   
+}
